151dd688c2a5e0e531f6a0df664fe0fd9d65af29
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-17 20:57:11 +0530

    feat: resend link if expired

--- src/app/api/verify-email/route.ts
+++ src/app/api/verify-email/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { parse } from 'url'; // Import the 'parse' function from the 'url' module
 import prisma from '@/app/libs/prismadb';
+import nodemailer from 'nodemailer'
 
 export async function GET(req: Request) {
   try{
@@ -59,6 +60,61 @@ export async function GET(req: Request) {
         });
         return NextResponse.json({success: true})
     }else{
+        // Send New Verification Link via email
+        const expiresAt = new Date();
+        expiresAt.setHours(expiresAt.getHours() + 24);
+
+        const userVerification = await prisma.userVerification.create({
+            data: {
+                userId: id,
+                role: role=="0"? "Creator" : "Brand",
+                link: link,
+                expiresAt: expiresAt
+            }
+        })
+        const user = await prisma.creator.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        // mail configrations
+        let config = {
+            service: "gmail",
+            auth: {
+                user: process.env.AUTH_EMAIL,
+                pass: process.env.AUTH_PASS
+            }
+        }
+
+        let transporter = nodemailer.createTransport(config);
+        const mailoptions = {
+            from: '"HyperEdge" <hyperedge@gmail.com>',
+            to: user?.email,
+            subject: "Verify your Email",
+            html: `
+                <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+                    <div style="margin:2px auto;width:90%;padding:0px 0">
+                    <div style="border-bottom:1px solid #eee">
+                        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">HyperEdge</a>
+                    </div>
+                    <p style="font-size:1.2em">Hi, <i><b>${user?.fullName}</b></i></p>
+                    <p style="font-size:1.1em">Thank you for registering with HyperEdge. To ensure the security of your account, we kindly ask you to verify your email address by clicking the button below. After verification, you can proceed to complete your profile and begin your journey on HyperEdge.</p>
+                    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 20px;color: #fff;border-radius: 4px;">
+                    <a href='${link}' style="color: white; text-decoration: none"> Verify Here </a>
+                    </h2>
+                    <p style="font-size:1em">Please note that the verification link will expire within 24 hours.</p>
+                    <p style="font-size:1em;">Regards,<br />HyperEdge</p>
+                    <hr style="border:none;border-top:1px solid #eee" />
+                    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+                        <p>HyperEdge Inc</p>
+                        <p>India</p>
+                    </div>
+                    </div>
+                </div>
+            `
+        }
+        await transporter.sendMail(mailoptions);
         return NextResponse.json({expired: true})
     }
   }catch(error){
