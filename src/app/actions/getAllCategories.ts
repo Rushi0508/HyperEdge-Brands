bf514b7f9f48645d3f4a514fd1d331599bd27c12
Rushi_0508 <rushigandhi14@gmail.com>
2024-01-08 22:11:23 +0530

    ts fixes

--- src/app/actions/getAllCategories.ts
+++ src/app/actions/getAllCategories.ts
@@ -5,7 +5,7 @@ export const getAllCategories = async ()=>{
     const indexArray = Object.keys((data.categories)).map((index,i) => ({
         value: i,
         label: index,
-        subLabels: data.categories[index]
+        subLabels: (data.categories as Record<string, string[]>)[index]
     }));
     return indexArray
 }
