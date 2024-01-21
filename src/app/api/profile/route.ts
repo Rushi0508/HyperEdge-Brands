90132f195142cf07ba3ecb6dfe4d1f8b94cbe80c
Rushi_0508 <rushigandhi14@gmail.com>
2024-01-21 18:15:27 +0530

    fixing imports

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import { parse } from "url";
+import prisma from '@/app/libs/prismadb'
 
 export async function POST(req: Request){
     try{
