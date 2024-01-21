126d3a57bce890ac021e22d7ec0ef2e956dea70a
Rushi_0508 <rushigandhi14@gmail.com>
2024-01-21 17:56:20 +0530

    tweaking prisma file

--- src/app/libs/prismadb.ts
+++ src/app/libs/prismadb.ts
@@ -4,7 +4,7 @@ declare global{
     var prisma: PrismaClient | undefined
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if(process.env.NODE_ENV!='production') globalThis.prisma=client
+const client = global.prisma || new PrismaClient({log: ["info"]});
+if(process.env.NODE_ENV!='production') global.prisma=client
 
 export default client;
