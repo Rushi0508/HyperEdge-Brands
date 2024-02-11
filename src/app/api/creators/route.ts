1797aac16138e8ad2f5fd5d1f151967a0d9c914b
Rushi_0508 <rushigandhi14@gmail.com>
2024-02-11 18:17:39 +0530

    feat: configured prisma accelerate

--- src/app/api/creators/route.ts
+++ src/app/api/creators/route.ts
@@ -1,11 +1,16 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from '@/app/libs/prismadb'
-import { parse } from "url";
 
 export async function POST(req: Request){
     try{
-        const creators = await prisma.creator.findMany();
+        const creators = await prisma.creator.findMany({
+            cacheStrategy:{
+                ttl: 3600,
+                swr: 500,
+            }
+        });
+        console.log(creators)
         return NextResponse.json({success: true, creators:creators})
     }catch(error){
         console.log(error)
