fbb692a8d4ef150232b4ff4ee258b14ec3f09fd8
Rushi_0508 <rushigandhi14@gmail.com>
2024-02-28 12:04:12 +0530

    Styling Fixes

--- src/app/collaborations/page.tsx
+++ src/app/collaborations/page.tsx
@@ -25,42 +25,46 @@ function page() {
     useEffect(() => {
         fetchCollaborations()
     }, [])
-    if (!collaborations) return <Loading />
     return (
-        <div className='grid grid-cols-4 gap-4'>
-            {
-                collaborations && collaborations.length > 0 ?
-                    collaborations?.map((collaboration: any) => (
-                        <>
-                            <Card key={collaboration.id}>
-                                <CardHeader>
-                                    <CardTitle>{collaboration.campaign.name}</CardTitle>
-                                    <CardDescription>{timeAgo(collaboration.createdAt)}</CardDescription>
-                                </CardHeader>
-                                <CardContent className='flex flex-col items-center'>
-                                    <div>
-                                        <Image alt='profile' width={0} height={0} sizes='100vw' style={{ overflowClipMargin: "unset" }} className="w-[150px] h-[150px] object-cover rounded-full" src={collaboration.creator.avatar} />
-                                    </div>
-                                    <div>
-                                        <p className='mt-2 font-semibold text-center tracking-wider'>{collaboration.creator.fullName}</p>
-                                        <div className="flex">
-                                            <Button variant={'link'} className='px-2' asChild>
-                                                <Link href={`/creator/${collaboration.creatorId}`}>View Profile</Link>
-                                            </Button>
-                                            <Button variant={'link'} className='px-2' asChild>
-                                                <Link href={`/campaign/${collaboration.campaignId}`}>View Campaign</Link>
-                                            </Button>
-                                        </div>
-                                    </div>
-                                </CardContent>
-                            </Card>
-                        </>
-                    )) :
-                    <div>
-                        <p className='text-xl text-center tracking-wide'>No collaborations yet</p>
-                    </div>
+        <>
+            <p className='text-center mb-4 font-bold text-2xl'>Collaborations</p>
+            {!collaborations ? <Loading /> :
+                <div className='grid grid-cols-4 gap-4'>
+                    {
+                        collaborations && collaborations.length > 0 ?
+                            collaborations?.map((collaboration: any) => (
+                                <>
+                                    <Card key={collaboration.id}>
+                                        <CardHeader>
+                                            <CardTitle>{collaboration.campaign.name}</CardTitle>
+                                            <CardDescription>{timeAgo(collaboration.createdAt)}</CardDescription>
+                                        </CardHeader>
+                                        <CardContent className='flex flex-col items-center'>
+                                            <div>
+                                                <Image alt='profile' width={0} height={0} sizes='100vw' style={{ overflowClipMargin: "unset" }} className="w-[150px] h-[150px] object-cover rounded-full" src={collaboration.creator.avatar} />
+                                            </div>
+                                            <div>
+                                                <p className='mt-2 font-semibold text-center tracking-wider'>{collaboration.creator.fullName}</p>
+                                                <div className="flex">
+                                                    <Button variant={'link'} className='px-2' asChild>
+                                                        <Link href={`/creator/${collaboration.creatorId}`}>View Profile</Link>
+                                                    </Button>
+                                                    <Button variant={'link'} className='px-2' asChild>
+                                                        <Link href={`/campaign/${collaboration.campaignId}`}>View Campaign</Link>
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </CardContent>
+                                    </Card>
+                                </>
+                            )) :
+                            <div>
+                                <p className='text-xl text-center tracking-wide'>No collaborations yet</p>
+                            </div>
+                    }
+                </div>
             }
-        </div>
+        </>
     )
 }
 
