422d2212a2e059542ab49358c30357df85952f22
Rushi_0508 <rushigandhi14@gmail.com>
2024-03-15 11:36:30 +0530

    Fixed useEffect dependency error

--- src/app/creator/[id]/page.tsx
+++ src/app/creator/[id]/page.tsx
@@ -9,49 +9,49 @@ import Sidebar from './components/Sidebar'
 import TitleBio from './components/TitleBio'
 import Collaborations from './components/Collaborations'
 
-function page({params}: {params: {id: string}}) {
-    const [dataLoading, setDataLoading] = useState(true)
-    const [creator, setCreator] = useState<any>(null)
+function page({ params }: { params: { id: string } }) {
+  const [dataLoading, setDataLoading] = useState(true)
+  const [creator, setCreator] = useState<any>(null)
 
-    const fetchCreator = async()=>{
-        try{
-            const {data} = await axios.post(`/api/creators/${params.id}`, {id: params.id})
-            if(data.hasOwnProperty('success') && data.creator){
-                setCreator(data.creator);
-            }else{
-              setCreator("")
-            }
-        }catch(error){
-            toast.error("Something went wrong");
-        }finally{
-            setDataLoading(false);
-        }
+  const fetchCreator = async () => {
+    try {
+      const { data } = await axios.post(`/api/creators/${params.id}`, { id: params.id })
+      if (data.hasOwnProperty('success') && data.creator) {
+        setCreator(data.creator);
+      } else {
+        setCreator("")
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setDataLoading(false);
     }
+  }
 
-    useEffect(()=>{
-        fetchCreator();
-    })
+  useEffect(() => {
+    fetchCreator();
+  }, [])
 
-    if(creator=="") return <NotFound/>
-    else if(dataLoading) return <Loading/>
+  if (creator == "") return <NotFound />
+  else if (dataLoading) return <Loading />
   return (
     <>
-    <div className="border-[1px] border-gray-300 rounded-2xl">
-      <Header user={creator}/>
-      <div className="flex">
-        <div className="w-[30%] border-r-[1px] border-gray-300">
-          <Sidebar user={creator}/>
-        </div>
-        <div className="w-[70%]">
-          <div className='py-5 px-10 border-b-[1px] border-gray-300'>
-            <TitleBio user={creator}/>
+      <div className="border-[1px] border-gray-300 rounded-2xl">
+        <Header user={creator} />
+        <div className="flex">
+          <div className="w-[30%] border-r-[1px] border-gray-300">
+            <Sidebar user={creator} />
           </div>
-          <div className='py-5 px-10 border-b-[1px] border-gray-300'>
-            <Collaborations user={creator}/>
+          <div className="w-[70%]">
+            <div className='py-5 px-10 border-b-[1px] border-gray-300'>
+              <TitleBio user={creator} />
+            </div>
+            <div className='py-5 px-10 border-b-[1px] border-gray-300'>
+              <Collaborations user={creator} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
